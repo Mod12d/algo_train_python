d16bf0a46f0f4e2d5b7a25440f73d49ac89b16fc
y10i <terumi8.neu@icloud.com>
2023-10-18 23:27:06 +0900

    Create base.py

---
+++ base.py
@@ -0,0 +1,8 @@
+def solution(n: int = 1000) -> int:
+
+   #return [e for e in range(3, n) if e % 3 == 0 or e % 5 == 0])
+  eturn [e for e in range(3, n)])
+
+
+if __name__ == "__main__":
+    print(solution(int(input().strip())))
